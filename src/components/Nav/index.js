868976bd0919ba867be3d877a7d3554eb17652f6
Josh Carter <joshcarter8400@gmail.com>
2020-12-19 09:43:49 -0500

    added onClick for header

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -14,7 +14,9 @@ function Nav() {
       description: "Fields, farmhouses, waterfalls, and the beauty of nature",
     },
   ];
-
+  function categorySelected(name) {
+    console.log(`${name} clicked`);
+  }
   return (
     <header>
       <h2>
@@ -36,7 +38,7 @@ function Nav() {
           </li>
           {categories.map((category) => (
             <li className="mx-1" key={category.name}>
-              <span>{category.name}</span>
+              <span onClick={categorySelected}>{category.name}</span>
             </li>
           ))}
         </ul>
