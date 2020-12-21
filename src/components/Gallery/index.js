226b4fe13993d1d6979a8aa9f8befd7d135ea4c8
Josh Carter <joshcarter8400@gmail.com>
2020-12-21 07:22:23 -0500

    trying to fix error

--- src/components/Gallery/index.js
+++ src/components/Gallery/index.js
@@ -4,6 +4,7 @@ import { capitalizeFirstLetter } from "../../utils/helpers";
 
 function Gallery({ currentCategory }) {
   const { name, description } = currentCategory;
+
   return (
     <section>
       <h1 data-testid="h1tag">{capitalizeFirstLetter(name)}</h1>
